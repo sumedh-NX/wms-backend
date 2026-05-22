3448da686519bb95333d0fac5c1c99b967e928f1
sumedh-NX <sumedh.nx@gmail.com>
2026-05-23 00:49:09 +0530

    Modularised the Files-BE

---
+++ src/customers/registry.js
@@ -0,0 +1,11 @@
+/**
+ * Customer Plugin Registry
+ * To add a new customer: create src/customers/<name>/index.js and add one entry here.
+ * To remove a customer: delete the folder and remove the entry here.
+ */
+const nitera = require('./nitera');
+const usui   = require('./usui');
+
+const CUSTOMER_REGISTRY = [nitera, usui];
+
+module.exports = CUSTOMER_REGISTRY;
